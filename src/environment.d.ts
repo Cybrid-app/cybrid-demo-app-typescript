b59009d4374513e1492eadc287def0a477f80455
Brandon Vrooman <brandon.vrooman@gmail.com>
2022-07-19 14:57:58 -0400

    Copy Typescript demo app from `cybrid-test-scripts` (#1)

---
+++ src/environment.d.ts
@@ -0,0 +1,15 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      ATTESTATION_SIGNING_KEY: string;
+      APPLICATION_CLIENT_ID: string;
+      APPLICATION_CLIENT_SECRET: string;
+      BANK_GUID: string;
+      BASE_URL: string;
+      TIMEOUT: string;
+      VERIFICATION_KEY_GUID: string;
+    }
+  }
+}
+
+export {};
